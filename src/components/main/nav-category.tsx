4cee446f5e159bf27e1113c6abc39f1b1036b9ea
xggz <yyimba@qq.com>
2025-12-13 23:12:25 +0800

    up

--- src/components/main/nav-category.tsx
+++ src/components/main/nav-category.tsx
@@ -13,7 +13,6 @@ import { Link } from "@/i18n/navigation"
 import { Category } from "@/types/api/category"
 import {
   Bug,
-  CodeXml,
   FileSliders,
   HeartPlus,
   HousePlug,
@@ -23,6 +22,7 @@ import {
   Smile,
   SquareFunction,
 } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 export function NavCategory() {
   const { isMobile } = useSidebar()
@@ -32,46 +32,55 @@ export function NavCategory() {
       id: "1",
       name: "文档",
       icon: FileSliders,
+      color: "blue-500",
     },
     {
       id: "2",
       name: "支持",
       icon: HeartPlus,
+      color: "green-500",
     },
     {
       id: "9",
       name: "公告",
       icon: Megaphone,
+      color: "red-500",
     },
     {
       id: "3",
       name: "聊天灌水",
       icon: MessageCircleHeart,
+      color: "indigo-500",
     },
     {
       id: "4",
       name: "插件",
       icon: HousePlug,
+      color: "orange-500",
     },
     {
       id: "5",
       name: "功能",
       icon: SquareFunction,
+      color: "fuchsia-500",
     },
     {
       id: "6",
       name: "用户体验",
       icon: Smile,
+      color: "teal-500",
     },
     {
       id: "7",
       name: "错误",
       icon: Bug,
+      color: "yellow-500",
     },
     {
       id: "8",
       name: "主题组件",
       icon: Palette,
+      color: "violet-500",
     },
   ])
 
@@ -83,7 +92,9 @@ export function NavCategory() {
           <SidebarMenuItem key={category.id}>
             <SidebarMenuButton asChild>
               <Link href={`/category/${category.id}`}>
-                {category.icon && <category.icon />}
+                {category.icon && (
+                  <category.icon className={cn(`text-${category.color}`)} />
+                )}
                 <span>{category.name}</span>
               </Link>
             </SidebarMenuButton>
--- src/types/api/category.ts
+++ src/types/api/category.ts
@@ -4,4 +4,5 @@ export interface Category {
   id: string
   name: string
   icon: LucideIcon
+  color: string
 }
