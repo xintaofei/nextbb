9d6eb3f7a0b0ff89131026e482db17713dfbabd5
xggz <yyimba@qq.com>
2025-12-18 16:04:11 +0800

    去掉 supabase 强依赖

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,41 +1,7 @@
 import createMiddleware from "next-intl/middleware"
-import { NextRequest, NextResponse } from "next/server"
-import { createServerClient, type CookieOptions } from "@supabase/ssr"
 import { routing } from "./i18n/routing"
 
-export default async function middleware(req: NextRequest) {
-  const res = createMiddleware(routing)(req)
-  const supabase = createServerClient(
-    process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return req.cookies
-            .getAll()
-            .map((c) => ({ name: c.name, value: c.value }))
-        },
-        setAll(cookies) {
-          cookies.forEach(
-            ({
-              name,
-              value,
-              options,
-            }: {
-              name: string
-              value: string
-              options: CookieOptions
-            }) => {
-              res.cookies.set({ name, value, ...options })
-            }
-          )
-        },
-      },
-    }
-  )
-  await supabase.auth.getSession()
-  return res
-}
+export default createMiddleware(routing)
 
 export const config = {
   matcher: ["/((?!api|_next|.*\\..*).*)"],
