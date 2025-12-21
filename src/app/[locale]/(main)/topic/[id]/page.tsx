78197e33b084af57b6b039a96bd0e3dba2e1d956
xintaofei <itpkcn@gmail.com>
2025-12-21 20:25:36 +0800

    upup

--- src/app/[locale]/(main)/topic/[id]/page.tsx
+++ src/app/[locale]/(main)/topic/[id]/page.tsx
@@ -311,7 +311,7 @@ export default function TopicPage() {
                   </TimelineStepsIcon>
                   <TimelineStepsContent className="border-b">
                     <div className="flex flex-row justify-between items-center w-full">
-                      <div className="flex flex-row gap-2">
+                      <div className="flex flex-row gap-2 items-center">
                         <Skeleton className="h-5 w-24" />
                         <Skeleton className="h-4 w-16" />
                       </div>
