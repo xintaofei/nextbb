d29a27a8ced2fc67a3047c6a64672e7df41d4c19
xintaofei <itpkcn@gmail.com>
2026-01-19 13:45:54 +0800

    首页主题列表

--- src/app/[locale]/(main)/[[...segments]]/page.tsx
+++ src/app/[locale]/(main)/[[...segments]]/page.tsx
@@ -72,7 +72,7 @@ export default function DynamicRoutePage() {
     if (apiQuery.tagId) qs.set("tagId", apiQuery.tagId)
     if (apiQuery.sort) qs.set("sort", apiQuery.sort)
     qs.set("page", String(pageIndex + 1))
-    qs.set("pageSize", "2")
+    qs.set("pageSize", "20")
     return `/api/topics?${qs.toString()}`
   }
 
