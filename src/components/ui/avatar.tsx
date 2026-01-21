41a118fdc8223548ab80c8456893a743a840ce14
xintaofei <itpkcn@gmail.com>
2026-01-21 23:37:14 +0800

    增加AvatarBadge

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -2,6 +2,7 @@
 
 import * as React from "react"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
+import { cva, type VariantProps } from "class-variance-authority"
 
 import { cn } from "@/lib/utils"
 
@@ -50,4 +51,43 @@ function AvatarFallback({
   )
 }
 
-export { Avatar, AvatarImage, AvatarFallback }
+const avatarBadgeVariants = cva(
+  "absolute flex items-center justify-center rounded-full bg-background text-xs ring-2 ring-background",
+  {
+    variants: {
+      position: {
+        "bottom-right": "-bottom-0.5 -right-0.5",
+        "bottom-left": "-bottom-0.5 -left-0.5",
+        "top-right": "-top-0.5 -right-0.5",
+        "top-left": "-top-0.5 -left-0.5",
+      },
+      size: {
+        sm: "size-3 text-[0.5rem]",
+        md: "size-4 text-[0.625rem]",
+        lg: "size-5 text-xs",
+      },
+    },
+    defaultVariants: {
+      position: "bottom-right",
+      size: "lg",
+    },
+  }
+)
+
+function AvatarBadge({
+  className,
+  position,
+  size,
+  ...props
+}: React.HTMLAttributes<HTMLSpanElement> &
+  VariantProps<typeof avatarBadgeVariants>) {
+  return (
+    <span
+      data-slot="avatar-badge"
+      className={cn(avatarBadgeVariants({ position, size, className }))}
+      {...props}
+    />
+  )
+}
+
+export { Avatar, AvatarImage, AvatarFallback, AvatarBadge, avatarBadgeVariants }
