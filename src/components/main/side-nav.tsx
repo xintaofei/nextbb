41bedc6f3b660e420dc77ba5ddbdc1eae4793f37
xintaofei <itpkcn@gmail.com>
2026-01-26 13:54:17 +0800

    upup

--- src/components/main/side-nav.tsx
+++ src/components/main/side-nav.tsx
@@ -4,7 +4,7 @@ import { SideNavContent } from "@/components/main/side-nav-content"
 
 export function SideNav() {
   return (
-    <aside className="hidden sm:flex flex-col sticky top-0 h-screen bg-background xl:w-52 transition-[width] duration-300 z-30 shrink-0">
+    <aside className="hidden sm:flex flex-col sticky top-0 h-screen bg-background xl:w-48 transition-[width] duration-300 z-30 shrink-0">
       <SideNavContent mode="desktop" />
     </aside>
   )
